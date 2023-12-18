0033f164bf5c81000a3c22c5d5efdb2a5822083d
yusuShin <tlsdbtn0507@naver.com>
2023-12-18 22:33:01 +0900

    [page]로그인 페이지 구현

--- front/src/router/Login.tsx
+++ front/src/router/Login.tsx
@@ -1,15 +1,25 @@
 import { Form } from 'react-router-dom'
 import css from '../css/login.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLock, faUser } from '@fortawesome/free-solid-svg-icons'
+
 
 const Login = () =>{ 
   return(
-    <Form>
-      <div className={css.div}>
-        <h1>login</h1>
-        <label htmlFor='id' id='id'><p>Id</p></label>
+    <div className={css.div}>
+      <Form className={css.form}>
+        <h1>Log In</h1>
+        <label htmlFor='id' id='id'className={css.font}>
+          <FontAwesomeIcon icon={faUser}/> Id
+        </label>
         <input type="text" name="id" id="id" />
-      </div>
-    </Form>
+        <label htmlFor='pw' id='pw'className={css.font}>
+          <FontAwesomeIcon icon={faLock}/> PW
+        </label>
+        <input type="password" name="pw" id="pw" />
+        <button className={css.btn}>로그인</button>
+      </Form>
+    </div>
   )
 }
 
