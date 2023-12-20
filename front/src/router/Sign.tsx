57d7033794437bf57b9a7d31995743ff67482e2b
yusuShin <tlsdbtn0507@naver.com>
2023-12-21 00:03:57 +0900

    [sign] css 및 로직 변경

--- front/src/router/Sign.tsx
+++ front/src/router/Sign.tsx
@@ -4,16 +4,19 @@ import css from '../css/sign.module.css'
 
 const Sign = () => {
 
-  
+  const doOtherThing = (e:React.FormEvent) =>{
+    e.preventDefault();
+    console.log('doing other')
+  };  
 
   return(
     <div className={css.total}>
-      <Form className={css.div}>
+      <Form className={css.div} method='post'>
           <h1>회원가입</h1> 
           <label htmlFor="userName">이름</label>
             <input type="userName" id='name' name='name'/>
           <label htmlFor="userId">아이디
-            <button className={css.checkBtn}>
+            <button onClick={doOtherThing} className={css.checkBtn}>
               아이디 중복 조회
             </button></label>
             <input type="userId" id='userId' name='userId'/>
@@ -23,7 +26,7 @@ const Sign = () => {
           <label htmlFor="pwCheck">비밀번호 확인</label>
             <input type="password" id='pwCheck' name='pwCheck'
               placeholder='비밀번호 확인'/>
-          <button className={css.sendBtn}>회원가입 완료</button>
+          <button type='submit' className={css.sendBtn}>회원가입 완료</button>
        </Form>
     </div>
   )
