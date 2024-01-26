3403847f895a9299e00763bca40fd2a76de6edec
yusuShin <tlsdbtn0507@naver.com>
2024-01-26 17:59:36 +0900

    [signup] 회원가입 예외 추가

--- back/src/user/dto/createUser.Dto.ts
+++ back/src/user/dto/createUser.Dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, Matches, MinLength } from "class-validator";
 
 export class CreateUserDto {
 
@@ -6,8 +6,20 @@ export class CreateUserDto {
   name: string;
   
   @IsNotEmpty()
+  @Matches(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, {
+    message:'아이디에 한글을 포함할 수 없습니다'
+  })
+  @MinLength(4, {
+    message:'아이디는 최소 4자 이상이여야합니다'
+  })
   userId: string
   
   @IsNotEmpty()
+  @MinLength(6, {
+    message:'비밀번호는 6자 이상이어야 합니다'
+  })
+  @Matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/, {
+    message:'비밀번호는 영문,숫자와 특수문자 포함해야합니다'
+  })
   pw: string;
 }
