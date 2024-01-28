722fcd5d2048ea3f56ce25b93556485ab3d192f2
yusuShin <tlsdbtn0507@naver.com>
2024-01-28 21:28:46 +0900

    [zustand] 로그인 zustand 설정

--- front/src/router/main.tsx
+++ front/src/router/main.tsx
@@ -1,6 +1,9 @@
+import { useEffect } from 'react'
 import LastToday from '../components/lastToday'
 import MyDiaries from '../components/myDiaries'
 import css from '../css/main.module.css'
+import userStore from '../store/user/userStore'
+import { useNavigate } from 'react-router'
 
 const Main = () => {
 
@@ -15,8 +18,18 @@ const Main = () => {
     return `${year} . ${month+1} . ${date} . ${days[day]}`
   }
 
-  
+  const { accessToken } = userStore(state => state);
 
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    
+    if (accessToken === '') {
+      alert('로그인 후 사용해주세요')
+      navigate('/login');
+    }
+
+  },[accessToken])
 
   return(
     <div className={css.total}>
--- front/src/store/user/userStore.ts
+++ front/src/store/user/userStore.ts
@@ -2,16 +2,20 @@ import { create } from "zustand";
 
 interface UserState  {
   pw: string,
-  pwCheck:boolean
-  setUserPw: (pw: string) => void
+  pwCheck: boolean,
+  accessToken:string,
+  setUserPw: (pw: string) => void,
+  setAccToken:(token:string)=>void
   setUserPWCheck : (check:boolean) =>void
 }
 
 const userStore = create<UserState>((set) => ({
   pw: '',
-  pwCheck:false,
+  pwCheck: false,
+  accessToken:'',
   setUserPw: (pw) => set(state => ({ pw })),
-  setUserPWCheck : (check)=>set(state => ({pwCheck:check}))
+  setAccToken:(accessToken)=>(set({accessToken})),
+  setUserPWCheck: (check) => set(state => ({ pwCheck: check })),
 }))
 
 export default userStore
