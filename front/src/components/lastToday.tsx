43edf8a0256768682acfb3ebe928a16a696dd53d
yusuShin <tlsdbtn0507@naver.com>
2024-02-03 22:56:12 +0900

    [diary] diary fetching

--- front/src/components/lastToday.tsx
+++ front/src/components/lastToday.tsx
@@ -1,13 +1,26 @@
 import css from '../css/laTod.module.css'
+import { diaryStore } from '../store/diary/diaryStore'
 import Diary from './diary'
+import NullDiary from './nullDiary';
 
-const LastToday = () =>{
+const LastToday = () => {
+  
+  const { diaries } = diaryStore(state => state);
+  
+  let content = <p>fetching</p>;
+
+  if (diaries.length !== 0) {
+    const lastDiary = diaries[0];
+    content = <Diary diaryInfo={lastDiary}/>
+  } else {
+    content = <NullDiary msg='작년의 오늘에 쓴 일기가'/>
+  }
 
   return(
-    <>
+    <div className={css.total}>
       <h5 className={css.title}>작년의 오늘</h5>
-      <Diary/>
-    </>
+      {content}
+    </div>
   )
 }
 
--- front/src/components/myDiaries.tsx
+++ front/src/components/myDiaries.tsx
@@ -1,10 +1,45 @@
+import { useQuery } from "react-query"
+import { getDiaries } from "../api/diary/diaryApi"
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+import Diary from "./diary";
+
+import css from '../css/diaryList.module.css'
+import { diaryStore } from "../store/diary/diaryStore";
+import NullDiary from "./nullDiary";
+
 const MyDiaries = () => {
 
+  const navigate = useNavigate();
+
+  const { data, isError, isFetching } = useQuery('diaries', getDiaries);
+
+  const { getDiaries: fetchingDiary ,diaries} = diaryStore(state => state);
+
+  useEffect(() => {
+    if (isError) {
+      alert('로그인 후 사용해 주세요!');
+      navigate('/login');
+      window.location.reload();
+    }
+    if (data) fetchingDiary(data)
+    
+  }, [isError,data]);
+
+  let content
+
+  if (diaries) content = diaries.map(e => <Diary key={e.id} diaryInfo={e} />);
+
+  if (diaries.length === 0) content = <NullDiary msg="아직 작성한 일기가" />;
+
+  if (isFetching) content = <p>데이터를 불러오는 중입니다...</p>;
 
   return(
   <>
-    <h5>나의 일기들</h5>
-    
+    <h5 className={css.h5}>나의 일기들</h5>
+    <div className={css.list}>
+      {content}
+    </div>
   </>
   )
 }
