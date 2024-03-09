d4433001af9a8c557a79954208aea984afa23e4a
yusuShin <tlsdbtn0507@naver.com>
2024-03-09 22:50:37 +0900

    [jest] front jest err fix

---
+++ front/jest.config.ts
@@ -0,0 +1,17 @@
+import type {Config} from 'jest';
+// import type Config from 'jest'
+
+const config: Config = {
+  moduleNameMapper: {
+    '^image![a-zA-Z0-9$_-]+$': 'GlobalImageStub',
+    '^[./a-zA-Z0-9$_-]+\\.png$': '<rootDir>/RelativeImageStub.js',
+    'module_name_(.*)': '<rootDir>/substituted_module_$1.js',
+    'assets/(.*)': [
+      '<rootDir>/images/$1',
+      '<rootDir>/photos/$1',
+      '<rootDir>/recipes/$1',
+    ],
+  },
+};
+
+export default config;
