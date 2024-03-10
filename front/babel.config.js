6d66d49a21f2d3ea22330e4aef57f285badcecea
yusuShin <tlsdbtn0507@naver.com>
2024-03-10 18:38:14 +0900

    [babel] install edit babel.config

---
+++ front/babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [
+    [
+      { targets: { node: 'current' } },
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/preset-typescript',
+    ]
+  ],
+};
+
