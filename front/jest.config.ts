b889e92d3a96b6ff0fd1123ad1f5301057ab0ccd
yusuShin <tlsdbtn0507@naver.com>
2024-03-10 23:09:16 +0900

    [jest] edit jest.config.ts

--- front/jest.config.ts
+++ front/jest.config.ts
@@ -92,9 +92,9 @@ const config: Config.InitialOptions = {
 
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {
-
-  // },
+  moduleNameMapper: {
+    'module_name_(.*)': '<rootDir>/substituted_module_$1.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
