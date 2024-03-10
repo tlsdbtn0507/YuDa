cae2dd9d84b3a720977df9bfdab9e93161619b13
yusuShin <tlsdbtn0507@naver.com>
2024-03-11 00:16:11 +0900

    [tsconfig] tsconfig edit

--- front/jest.config.ts
+++ front/jest.config.ts
@@ -94,6 +94,7 @@ const config: Config.InitialOptions = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     'module_name_(.*)': '<rootDir>/substituted_module_$1.js',
+     '\\.css$': 'identity-obj-proxy',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
