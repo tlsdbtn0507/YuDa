822b37cf6701039cd5b66c1e340f3dc198a4a7ac
yusuShin <tlsdbtn0507@naver.com>
2024-03-10 21:40:12 +0900

    [jest] edit jest.config

--- front/jest.config.ts
+++ front/jest.config.ts
@@ -19,13 +19,14 @@ const config: Config.InitialOptions = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-
+  collectCoverageFrom: [
+      "**/*.(t|j)s"
+  ],
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: "../coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -74,15 +75,13 @@ const config: Config.InitialOptions = {
 
   // An array of directory names to be searched recursively up from the requiring module's location
   moduleDirectories: [
-    "node_modules"
+    "node_modules",
+    "src"
   ],
 
   // An array of file extensions your modules use
   moduleFileExtensions: [
     "js",
-    "mjs",
-    "cjs",
-    "jsx",
     "ts",
     "tsx",
     "json",
