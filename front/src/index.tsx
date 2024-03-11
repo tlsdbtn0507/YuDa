ce13230368f66daf05742d9fe25c7a28839bbbff
yusuShin <tlsdbtn0507@naver.com>
2024-03-11 23:13:39 +0900

    [file] delete test folder

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../src/css/index.css';
 import App from './App';
-import reportWebVitals from './test/reportWebVitals';
+import reportWebVitals from '../reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
