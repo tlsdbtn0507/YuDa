e3cf2d95acb843b245d2091821e11622fdea2cb5
yusuShin <tlsdbtn0507@naver.com>
2024-03-25 00:11:03 +0900

    [deploy] env name edit

--- back/src/configs/typeorm.config.ts
+++ back/src/configs/typeorm.config.ts
@@ -5,11 +5,11 @@ const dbConfig = config.get('db');
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.HOST || dbConfig.host,
-  port: +process.env.PORT || dbConfig.port,
-  username: process.env.USERNAME || dbConfig.username,
-  password: process.env.PASSWORD || dbConfig.password,
-  database: process.env.DATABASE || dbConfig.database,
+  host: process.env.POSTGRES_HOST || dbConfig.host,
+  port: +process.env.POSTGRES_PORT || dbConfig.port,
+  username: process.env.POSTGRES_USERNAME || dbConfig.username,
+  password: process.env.POSTGRES_PASSWORD || dbConfig.password,
+  database: process.env.POSTGRES_DATABASE || dbConfig.database,
   entities: [__dirname + "/../**/*.entity.{js,ts}"],
-  synchronize : !!process.env.SYNCHRONIZE || dbConfig.synchronize
+  synchronize : !!process.env.POSTGRES_SYNCHRONIZE || dbConfig.synchronize
 }
