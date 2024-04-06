229c765a3c8d3e7107a6796ff888d342018b65a1
yusuShin <tlsdbtn0507@naver.com>
2024-04-06 23:28:24 +0900

    [test] edit test

--- front/App.test.tsx
+++ front/App.test.tsx
@@ -5,6 +5,6 @@ import '@testing-library/jest-dom'
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Yuda');
+  const linkElement = screen.getByText('YuDa');
   expect(linkElement).toBeInTheDocument();
 });
