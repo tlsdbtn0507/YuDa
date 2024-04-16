b60fe688ccb4f191b6e662758c37bc2c38107c9d
yusuShin <tlsdbtn0507@naver.com>
2024-04-16 23:07:41 +0900

    [nginx] edit nginx setting and api

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_SERVER,
+  baseURL: process.env.REACT_APP_SERVER + '/api',
 })
 
 API.defaults.withCredentials = true
