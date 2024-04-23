5978a6739d292ba1a67146364f7fb3d20a31ea0d
yusuShin <tlsdbtn0507@naver.com>
2024-04-23 17:04:02 +0900

    [back] add pem file

--- back/src/configs/typeorm.config.ts
+++ back/src/configs/typeorm.config.ts
@@ -1,5 +1,6 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as config from 'config';
+import fs from 'fs'
 
 const dbConfig = config.get('db');
 
@@ -11,5 +12,13 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   password: process.env.POSTGRES_PASSWORD || dbConfig.password,
   database: process.env.POSTGRES_DATABASE || dbConfig.database,
   entities: [__dirname + "/../**/*.entity.{js,ts}"],
-  synchronize : !!process.env.POSTGRES_SYNCHRONIZE || dbConfig.synchronize
+  synchronize: !!process.env.POSTGRES_SYNCHRONIZE || dbConfig.synchronize,
+  ssl: {
+    ca:fs.readFileSync('../../../ap-northeast-2-bundle')
+  },
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 }
