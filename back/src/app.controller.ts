d0326c3e39cf553f07771a0ea08103985ba92e6e
yusuShin <tlsdbtn0507@naver.com>
2024-05-08 17:46:15 +0900

    [back] 배포시 테스트 api 추가

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+// @Controller()
+@Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
   }
