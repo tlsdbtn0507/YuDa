520d32c9a3943bc504598032b716cf960e0d6679
yusuShin <tlsdbtn0507@naver.com>
2024-05-19 00:26:06 +0900

    [azure] 백엔드 배포 과정 9

--- back/src/configs/typeorm.config.ts
+++ back/src/configs/typeorm.config.ts
@@ -11,7 +11,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   password: process.env.POSTGRES_PASSWORD || dbConfig.password,
   database: process.env.POSTGRES_DATABASE || dbConfig.database,
   entities: [__dirname + "/../**/*.entity.{js,ts}"],
-  synchronize: !!process.env.POSTGRES_SYNCHRONIZE || dbConfig.synchronize,
+  synchronize: process.env.POSTGRES_SYNCHRONIZE || dbConfig.synchronize,
   // ... process.env.NODE_ENV === 'production' && sslConfig  
 }
 
